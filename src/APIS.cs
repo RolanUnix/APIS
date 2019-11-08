2c99b0f814f04c3a729d3a0e6c7cce781db6fe3f
Ролан Логинов <rolanlog@mail.ru>
2019-11-08 14:13:46 +0300

    Реализовано принятие запросов, обработка через пользовательские методы и отправка ответа

--- src/APIS.cs
+++ src/APIS.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
-using APIS.Packet;
+using APIS.Exceptions;
+using APIS.Packets;
 
 namespace APIS
 {
@@ -12,20 +16,20 @@ namespace APIS
     {
         public delegate Response UriHandler(Request request);
 
-        private Dictionary<string, UriHandler> _handlers;
+        private readonly Dictionary<string, UriHandler> _handlers;
         private readonly IPAddress _bindAddress;
         private readonly int _port;
         private readonly bool _multiThreading;
-        private readonly bool _serverStatus;
-        private readonly object _queueLocker;
+        private readonly bool _debug;
+        private bool _serverStatus;
         private Thread _serverThread;
-        private Thread _queueThread;
-        private Queue _queueRequests;
 
         private TcpListener _serverListener;
 
         public UriHandler this[string uri]
         {
+            get => _handlers.ContainsKey(uri) ? _handlers[uri] : null;
+
             set
             {
                 if (_handlers.ContainsKey(uri)) _handlers[uri] = value;
@@ -33,26 +37,19 @@ namespace APIS
             }
         }
 
-        public WebServer(IPAddress bindAddress, int port, bool multiThreading = true)
+        public WebServer(IPAddress bindAddress, int port, bool multiThreading = true, bool debug = false)
         {
+            _handlers = new Dictionary<string, UriHandler>();
             _bindAddress = bindAddress;
             _port = port;
             _multiThreading = multiThreading;
             _serverStatus = false;
-            if (!multiThreading)
-            {
-                _queueRequests = new Queue();
-                _queueLocker = new object();
-            }
+            _debug = debug;
         }
 
         public void Start()
         {
-            if (!_multiThreading)
-            {
-                _queueThread = new Thread(HandlerQueue);
-                _queueThread.Start();
-            }
+            _serverStatus = true;
 
             _serverListener = new TcpListener(_bindAddress, _port);
             _serverListener.Start();
@@ -61,27 +58,73 @@ namespace APIS
             _serverThread.Start();
         }
 
+        public void Stop()
+        {
+            _serverThread.Abort();
+            _serverStatus = false;
+            _serverListener.Stop();
+        }
+
         private void HandlerServer()
         {
             while (_serverStatus)
             {
                 try
                 {
+                    var client = _serverListener.AcceptTcpClient();
 
+                    if (_multiThreading)
+                    {
+                        new Thread(_ => HandlerClient(client)).Start();
+                    }
+                    else
+                    {
+                        HandlerClient(client);
+                    }
                 }
-                catch (Exception e)
+                catch
                 {
-
+                    // ignored
                 }
             }
         }
 
-        private void HandlerQueue()
+        private void HandlerClient(TcpClient client)
         {
-            while (_serverStatus)
+            do
             {
-                Thread.Sleep(1);
-            }
+                try
+                {
+                    var buffer = new byte[8 * 1024 * 1024];
+                    var bytesAccepted = client.Client.Receive(buffer);
+
+                    buffer = buffer.Take(bytesAccepted).ToArray();
+
+                    if (buffer.Length == 0) continue;
+
+                    var packet = Request.Parse(buffer);
+
+                    var handler = this[packet.Uri];
+
+                    if (handler == null) throw new NotFoundException();
+
+                    client.Client.Send(handler.Invoke(packet).AsHttp());
+                }
+                catch (Exception e)
+                {
+                    HttpException error;
+
+                    if (e is HttpException) error = (HttpException)e;
+                    else error = new InternalServerException(_debug ? e.ToString() : null);
+
+                    if (client.Connected)
+                    {
+                        client.Client.Send(error.AsHttp());
+                    }
+                }
+            } while (_multiThreading && _serverStatus && client.Connected);
+
+            client.Close();
         }
     }
 }
